fe9c62ad39bbc7ca4f1b440e06bc65a1e049e404
abhijeet_Kumar <abhijeet.rts@gmail.com>
2022-03-22 17:19:41 +0530

    problem

--- src/_dsa/_0_LeetCode_Village/Math/_1_Add_2_number.java
+++ src/_dsa/_0_LeetCode_Village/Math/_1_Add_2_number.java
@@ -3,15 +3,26 @@ package _dsa._0_LeetCode_Village.Math;
 
 public class _1_Add_2_number {
     /* Problem Statement: 
-    * An ugly number is a positive integer that is divisible by a, b, or c.
-    * Given four integers n, a, b, and c, return the nth ugly number.    
+    * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+    * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    * Example: 
+    * l1: 2->4->3 and  l2: 5->6->4 
+    * result: 7->0->8
     */
     public static void main(String[] args) {
 
     }
     
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
+        /* Logic:
+        * add l1.val + l2.val + carry
+        * if total > 10... then take 0th digit number
+        * and make carry as total/10
+        *
+        * In last if carry > 0 ... add that to last NODE.
+        */
         ListNode curr = new ListNode(-1);
         ListNode res = curr;
         
