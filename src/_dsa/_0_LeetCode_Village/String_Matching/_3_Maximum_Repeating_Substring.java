d3dbf764462d5acb37b379fb9fc50bd0a773db45
Abhijeet Kumar <abhijeet.kumar@hashedin.com>
2022-04-14 04:13:48 +0530

    LC-Village files added

--- src/_dsa/_0_LeetCode_Village/String_Matching/_3_Maximum_Repeating_Substring.java
+++ src/_dsa/_0_LeetCode_Village/String_Matching/_3_Maximum_Repeating_Substring.java
@@ -3,12 +3,13 @@ package _dsa._0_LeetCode_Village.String_Matching;
 public class _3_Maximum_Repeating_Substring {
     // Question: https://leetcode.com/problems/maximum-repeating-substring/submissions/
 
+    // Edge Case: it is a substring, so in 'ababc' & 'ab' will work as... abab is repeating
+    //              but in 'abcab', ab is only there.. becoz in 2 times ab is not substring... it will work in sub-sequence
     public static void main(String[] args) {
-
+        System.out.println(maxRepeating("abcab","ab"));
     }
 
     public static int maxRepeating(String sequence, String word) {
-
 
         int count = 0;
 
