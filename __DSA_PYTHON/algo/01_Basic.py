9f5d7a2b50ae504ac07d20196a9ec9f62ef78aa4
Abhijeet-14 <abhijeet.rts@gmail.com>
2024-01-30 20:56:37 +0530

    updated

---
+++ __DSA_PYTHON/algo/01_Basic.py
@@ -0,0 +1,21 @@
+""" ALGORITHMS:
+=== Array ===
+- Binary Search
+- Kadane's 
+- Heap-Sort
+- Merge-Sort
+- Prefix & Sufix Sum
+=== Matrix ===
+- Lee 
+
+=== Graph ===
+- BFS
+- DFS
+- Dijkstra 
+- Floyd-Warshall 
+- Bellman-Ford 
+- Kruskal 
+- Topological 
+- Flood Fill 
+- Detect Cycle in LL 
+"""
