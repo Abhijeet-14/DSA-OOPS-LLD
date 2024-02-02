4a29ecc61c98af3afd70b0e8eacefbee933ce22a
Abhijeet-14 <abhijeet.rts@gmail.com>
2024-02-02 09:47:36 +0530

    add

--- __DSA_PYTHON/_LLD_PYTHON/Restaurants/zmain.py
+++ __DSA_PYTHON/_LLD_PYTHON/Restaurants/zmain.py
@@ -3,9 +3,7 @@
 
 import api
 
-import views
 import repository
-import models
 
 user_db = repository.UserRepo.create_user_table()
 restaurant_db = repository.RestaurantRepo.create_restaurant_table()
@@ -16,7 +14,6 @@ user_service = api.UserApi.create_user_service()
 user_1 = user_service.add_user(1, "ABC")
 user_2 = user_service.add_user(2, "XYZ")
 print(user_1, user_2)
-print(repository.UserRepo().get_all_users())
 
 restaurant_service = api.RestaurantAPI.create_restaurant_service()
 
