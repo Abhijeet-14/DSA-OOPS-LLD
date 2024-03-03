39f177283544a2cf66ab2c94c9f77be136714fab
Abhijeet-14 <abhijeet.rts@gmail.com>
2024-03-04 05:17:06 +0530

    ad

--- __DSA_PYTHON/_LLD_PYTHON/02-Concepts/17-State-DP.py
+++ __DSA_PYTHON/_LLD_PYTHON/02-Concepts/17-State-DP.py
@@ -6,7 +6,144 @@
 - 
 
 - Design LLD of Vending Machine - V.V.I
+    -    Cold Drink Vending Machine   
+        --------------------------------  
+        |  Coke    |  Pepsi   |  7UP   |  
+        |----------|----------|--------|  
+        |   Sprite |  Fanta   | Water  |  
+        |----------|----------|--------|  
+        |   Juice  |  Ice Tea |        |  
+        |------------------------------|  
+        |    Insert Money and Select   |  
+        |    the Drink of Your Choice  |  
+        |------------------------------|  
+
     - clarify requirement
-    - 
+        - it has Item(name, code, price, is_available)
+        - it has Item-Shelf
+        - insert cash button
+        - insert cash (Coin)
+        - select product button - 102
+        - if inserted cash > product price -> return change
+        - if inserted cash < product price -> raise Exception("Insuffient fund")
+        - delivery product -> Product Dischange
+        - cancel button -> return change -> Idle
+
+    - Happy FLow:
+        - turn on VM
+        - client descide Want Fanta -
+        - insert cash button -
+        - insert put coins
+        - select product
+        - check price -> return Change || raise Exception
+        - submit_button
+        - deliver product
+        - cancel request.
+
+        - STATE WISE
+            - turn_on --> Idle State
+
+            - insert cash button() --> Has_Money_State
+            - insert cash coint --> Has_Money_state
+                - cancel coin --> refund --> Idle state
+
+            - select product --> Selection_state
+                - cancel product --> refund --> Idle state
+
+            - submit product --> check Price vs Product --> calculate change -> Delivery State
     
+            - Delivery product --> Delier Product -> return change
+                -> Idle State
+
+                
+        - So, vending machine -- State Wise Operation
+            - Idle STATE:
+                - Press, Insert, Cash button 
+            - HasChange STATE:
+                - Inner coin
+                - select product
+                - cancel button -> Idel state
+            - Selection STATE:
+                - chosse product
+                - return change
+                - cancel/refund product -> Idel state
+            - Dilever STATE
+                - deliver product
+                - Idle state 
+
+    - Example TV:
+        - State Wise Operation:
+            - Off STATE:
+                - turn On TV.
+            - On STATE:
+                - change channel
+                - change volume
+                - turn off TV
+"""
+
+################
+# Solution
+################
+"""
+- Product
+- interface - State (all possible operation [op1, op2, op3, op4])
+- concreate class - State1 [op1(imp), op2, op3, op4,...]
+    - implement Strictly to operation realted to it.
+    - else -> throw exception or default.
+- concreate class - State2 [op1, op2(imp), op3, op4,...]
+- concreate class - State3 [op1, op2, op3(imp), op4,...]
+- concreate class - State4 [op1, op2, op3, op4(imp),...]
+- .....
+
+- Product 'HAS_A' State
+"""
+
+################
+# UML
+################
+"""
+- Vending machine
+- interface State -> 'Has_A' Vending machine
+    - press insert cash button
+    - insert coin
+    - submit button
+    - select product
+    - get product
+    - cancel
+- concreate class Idle 'IS_A' State 
+    - press insert cash button - logic
+    - insert coin -> throw exc
+    - submit button -> throw exc
+    - select product -> throw exc
+    - get product -> throw exc
+    - cancel
+- concreate class HasMoney 'IS_A' State
+    - press insert cash button - logic
+    - insert coin -> throw exc
+    - submit button -> throw exc
+    - select product -> throw exc
+    - get product -> throw exc
+    - cancel
+- concreate class Selection 'IS_A' State
+    - press insert cash button - logic
+    - insert coin -> throw exc
+    - submit button -> throw exc
+    - select product -> throw exc
+    - get product -> throw exc
+    - cancel
+- concreate class Deliver 'IS_A' State
+    - press insert cash button - logic
+    - insert coin -> throw exc
+    - submit button -> throw exc
+    - select product -> throw exc
+    - get product -> throw exc
+    - cancel
+"""
+
+
+""" Diff bw State-DP & Chain Of Responsibilit-DP
+- State DP:
+    - object behavior change -- when its internal state changes
+- Chain Of Responsibility:
+    - it PASSES request along chain of handlers
 """
---
+++ __DSA_PYTHON/_LLD_PYTHON/02-Concepts/20-Vending-Macine-Design.py
@@ -0,0 +1,374 @@
+"""
+Design A Vending Machine
+"""
+
+from enum import Enum, auto
+
+
+class CoinType(Enum):
+    ONE = 1
+    TWO = 2
+    FIVE = 5
+    TEN = 10
+
+
+class Coin:
+    def __init__(self, coin_type: CoinType):
+        self.coin_type = coin_type
+
+    def __str__(self):
+        return self.coin_type.value
+
+
+class ItemType(Enum):
+    COKE = auto()
+    PEPSI = auto()
+    SPRITE = auto()
+
+    def __str__(self):
+        return self.name
+
+
+class Item:
+    def __init__(self, item_type: ItemType, price):
+        self.item_type = item_type
+        self.price = price
+
+
+class ItemShelf:
+    def __init__(self, item: Item, code, stock: int):
+        self.item: Item = item
+        self.code = code
+        self.stock = stock
+
+    def deliver_item(self):
+        if self.stock != 0:
+            self.stock -= 1
+            return
+        print(f"Stock is already empty for {self.item.item_type}")
+
+    def is_out_of_stock(self):
+        return not bool(self.stock)
+
+
+class Inventory:
+    def __init__(self):
+        self.item_shelfes: list[ItemShelf] = []
+
+    def add_item_shelfes(self, item_shelf: ItemShelf):
+        self.item_shelfes.append(item_shelf)
+
+    def is_inventory_empty(self):
+        return not bool(len(self.item_shelfes))
+
+
+from abc import ABC, abstractmethod
+
+
+class StateInterface(ABC):
+    def __init__(self, name, default_err_msg):
+        self.name = name
+        self.default_err_msg = default_err_msg
+
+    def raise_default_err(self):
+        print(self.default_err_msg)
+
+    @abstractmethod
+    def insert_cash_button(self, machine):
+        self.raise_default_err()
+
+    @abstractmethod
+    def insert_coin(self, machine, coin):
+        self.raise_default_err()
+
+    @abstractmethod
+    def select_product_button(self, machine):
+        self.raise_default_err()
+
+    @abstractmethod
+    def choose_product(self, product_code, machine):
+        self.raise_default_err()
+
+    @abstractmethod
+    def get_product(self):
+        self.raise_default_err()
+
+    @abstractmethod
+    def get_change(self):
+        self.raise_default_err()
+
+    @abstractmethod
+    def deliver_product_to_client(self, machine):
+        self.raise_default_err()
+
+    @abstractmethod
+    def cancel_button(self, machine):
+        self.raise_default_err()
+
+    @abstractmethod
+    def refund_change(self):
+        self.raise_default_err()
+
+    def __str__(self):
+        return f"\nVending Machine state: {self.name}"
+
+
+class VendingMachine:
+    def __init__(self, inventory: Inventory, state: StateInterface):
+        self.inventory: Inventory = inventory
+        self.coin_list: list[Coin] = []
+        self.state: StateInterface = state
+        self.product_to_be_deliver: Item = None
+
+    def get_vending_machine_state(self):
+        return self.state
+
+    def set_vending_machine_state(self, state: StateInterface):
+        self.state = state
+
+    def get_vending_machine_inventory(self):
+        return self.inventory
+
+    def set_vending_machine_inventory(self, inventory: Inventory):
+        self.inventory = inventory
+
+    def get_vending_machine_coin_list(self):
+        return self.coin_list
+
+    def set_vending_machine_coin_list(self, coin_list: Inventory):
+        self.coin_list = coin_list
+
+    def get_sum_of_coins(self) -> int:
+        return sum(coin.__str__() for coin in self.coin_list)
+
+    def refund_amount(self) -> int:
+        amount_to_refund = self.get_sum_of_coins()
+        self.set_vending_machine_coin_list([])
+        self.set_vending_machine_state(IdleSTATE("IDLE"))
+        return amount_to_refund
+
+    def set_product_to_deliver(self, item):
+        self.product_to_be_deliver = item
+
+    def deliver_product(self):
+
+        for item_shelf in self.inventory.item_shelfes:
+            if item_shelf.item == self.product_to_be_deliver:
+                item_shelf.deliver_item()
+                self.set_vending_machine_coin_list([])
+                self.set_vending_machine_state(IdleSTATE)
+                print(
+                    f"Please collect your product: {self.product_to_be_deliver.item_type}"
+                )
+                return
+        print("Selected product is out of stock")
+
+
+class DeliverSTATE(StateInterface):
+    def __init__(self, name):
+        super().__init__(name, "Deliver product is in progress")
+
+    def insert_cash_button(self, machine: VendingMachine):
+        super().insert_cash_button()
+
+    def insert_coin(self, machine: VendingMachine, coin: Coin):
+        super().insert_coin()
+
+    def select_product_button(self, machine: VendingMachine):
+        super().select_product_button()
+
+    def choose_product(self, product_code, machine):
+        super().choose_product()
+
+    def get_product(self):
+        super().get_product()
+
+    def deliver_product_to_client(self, machine: VendingMachine):
+        machine.deliver_product()
+
+    def get_change(self):
+        super().get_change()
+
+    def cancel_button(self, machine: VendingMachine):
+        super().cancel_button(machine)
+        print("Cancelling not allowed")
+
+    def refund_change(self, machine: VendingMachine):
+        super().refund_change()
+        print("Refund not allowed")
+
+
+class SelectProductSTATE(StateInterface):
+    def __init__(self, name):
+        super().__init__(name, "Select product is in progress")
+
+    def insert_cash_button(self, machine: VendingMachine):
+        super().insert_cash_button()
+
+    def insert_coin(self, machine: VendingMachine, coin: Coin):
+        super().insert_coin()
+
+    def select_product_button(self):
+        super().select_product_button()
+
+    def choose_product(self, product_code, machine: VendingMachine):
+        selected_item: Item = None
+        for item_shelf in machine.inventory.item_shelfes:
+            if item_shelf.code == product_code:
+                selected_item = item_shelf.item
+                break
+
+        # check totol amount paid vs item price
+        total_amount_paid = machine.get_sum_of_coins()
+        if selected_item.price > total_amount_paid:
+            print(
+                f"Insufficent amount paid, required {selected_item.price}. Order cancelled"
+            )
+            self.refund_change(machine)
+            return
+
+        machine.set_product_to_deliver(selected_item)
+
+        amount_to_return = self.get_change(total_amount_paid, machine)
+        print("Returing amount: ", amount_to_return)
+
+        machine.set_vending_machine_state(DeliverSTATE("DELIVER_PRODUCT"))
+
+    def get_product(self):
+        super().get_product()
+
+    def deliver_product_to_client(self, machine: VendingMachine):
+        return super().deliver_product_to_client(machine)
+
+    def get_change(self, total_amount_paid: int, machine: VendingMachine):
+        return abs(machine.product_to_be_deliver.price - total_amount_paid)
+
+    def cancel_button(self, machine: VendingMachine):
+        print("Cancelling order")
+        return self.refund_change(machine)
+
+    def refund_change(self, machine: VendingMachine):
+        amount_to_be_refund = machine.refund_amount()
+        print(f"Refunding the amount: {amount_to_be_refund}")
+
+
+class HasMoneySTATE(StateInterface):
+    def __init__(self, name):
+        super().__init__(name, "Insert Coin operation is in progress")
+
+    def insert_cash_button(self, machine):
+        return super().insert_cash_button(machine)
+
+    def insert_coin(self, machine: VendingMachine, coin: Coin):
+        print(f"{coin.coin_type} accpeted")
+        machine.get_vending_machine_coin_list().append(coin)
+
+    def select_product_button(self, machine: VendingMachine):
+        print("Activating 'select product button'")
+        machine.set_vending_machine_state(SelectProductSTATE("SELECT_PRODUCT"))
+
+    def choose_product(self, product_code, machine):
+        return super().choose_product(product_code, machine)
+
+    def get_product(self):
+        return super().get_product()
+
+    def get_change(self):
+        return super().get_change()
+
+    def deliver_product_to_client(self, machine: VendingMachine):
+        return super().deliver_product_to_client(machine)
+
+    def cancel_button(self, machine: VendingMachine):
+        print("Cancelling order")
+        return self.refund_change(machine)
+
+    def refund_change(self, machine: VendingMachine):
+        amount_to_be_refund = machine.refund_amount()
+        print(f"Refunding the amount: {amount_to_be_refund}")
+
+
+class IdleSTATE(StateInterface):
+    def __init__(self, name):
+        self.name = name
+        self.default_err_msg = "Machine is IDLE, click Insert Cash Button first"
+
+    def insert_cash_button(self, machine: VendingMachine):
+        machine.set_vending_machine_coin_list([])
+        machine.set_vending_machine_state(HasMoneySTATE("HAS_MONEY"))
+
+    def get_change(self):
+        return super().get_change()
+
+    def get_product(self):
+        return super().get_product()
+
+    def select_product_button(self, machine):
+        return super().select_product_button(machine)
+
+    def choose_product(self, product_code, machine):
+        return super().choose_product(product_code, machine)
+
+    def cancel_button(self, machine):
+        return super().cancel_button(machine)
+
+    def refund_change(self):
+        return super().refund_change()
+
+    def deliver_product_to_client(self, machine: VendingMachine):
+        return super().deliver_product_to_client(machine)
+
+    def insert_coin(self, machine, coin):
+        return super().insert_coin(machine, coin)
+
+
+def vending_machine_client():
+    coin_ONE = Coin(CoinType.ONE)
+    coin_TWO = Coin(CoinType.TWO)
+    coin_FIVE = Coin(CoinType.FIVE)
+    coin_TEN = Coin(CoinType.TEN)
+
+    item_COKE = Item(ItemType.COKE, 10)
+    item_PEPSI = Item(ItemType.PEPSI, 12)
+    item_SPRITE = Item(ItemType.SPRITE, 17)
+
+    item_shelf_one = ItemShelf(item_COKE, 101, 3)
+    item_shelf_two = ItemShelf(item_PEPSI, 102, 1)
+    item_shelf_three = ItemShelf(item_SPRITE, 102, 0)
+
+    inventory = Inventory()
+
+    inventory.add_item_shelfes(item_shelf_one)
+    inventory.add_item_shelfes(item_shelf_two)
+    inventory.add_item_shelfes(item_shelf_three)
+
+    machine = VendingMachine(inventory, IdleSTATE("IDLE"))
+    print("Vending Machine Inventory is ready")
+
+    current_state: StateInterface = machine.get_vending_machine_state()
+    print(current_state)
+    current_state.insert_cash_button(machine)
+
+    current_state = machine.get_vending_machine_state()
+    print(current_state)
+    current_state.insert_coin(machine, coin_TEN)
+    current_state.insert_coin(machine, coin_FIVE)
+
+    current_state = machine.get_vending_machine_state()
+    print(current_state)
+    current_state.select_product_button(machine)
+    # current_state.cancel_button(machine)
+
+    current_state = machine.get_vending_machine_state()
+    print(current_state)
+    current_state.choose_product(102, machine)
+
+    current_state = machine.get_vending_machine_state()
+    print(current_state)
+    current_state.deliver_product_to_client(machine)
+
+    current_state = machine.get_vending_machine_state()
+    print(current_state)
+
+
+vending_machine_client()
