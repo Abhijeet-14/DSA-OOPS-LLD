78f3b0df17a50e9a6ed22b074b6e9f3b895ab23a
abhijeet-14 <abhijeet.rts@gmail.com>
2024-04-26 12:32:56 +0530

    ad

--- __DSA_PYTHON/03_LLD_PYTHON/02-BEFORE INTERVIEW.py
+++ __DSA_PYTHON/03_LLD_PYTHON/02-BEFORE INTERVIEW.py
@@ -1,6 +1,8 @@
 """
 1. Clarify & Gather Requiements
-2. Validate Assumption
-3. Use-Case Diagram -> Class Diagram -> Behavior Diagram
+    i. Find the flow
+    ii. Define the Scope
+2. List down object
+3. define Relationship bw objects
 4. Code -- with Design Patterns & OOPs & SOLID
 """
